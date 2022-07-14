a8f63cbca81d0e748806e9e63d0d7f6e08fe85fd
removedporn <86824510+removedporn@users.noreply.github.com>
2022-07-15 00:56:21 +0800

    Delete falcon_oauth directory

--- falcon_oauth/__init__.py
+++
@@ -1,7 +0,0 @@
-"OAuth for Falcon."
-VERSION = (0, 0, 0)
-
-__author__ = 'Yohan Boniface'
-__contact__ = "yohan.boniface@data.gouv.fr"
-__homepage__ = "https://github.com/yohanboniface/falcon-oauth"
-__version__ = ".".join(map(str, VERSION))
--- falcon_oauth/provider/oauth2/__init__.py
+++
@@ -1,4 +0,0 @@
-from .provider import OAuthProvider
-from .validator import OAuthValidator
-
-__all__ = ('OAuthProvider', 'OAuthValidator')
